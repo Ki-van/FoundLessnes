4a1b00901d36cdc9f51bd908aacd5c55db6156a1
Ki-van <kirill.ivanin00@yandex.ru>
2021-04-26 02:36:46 +0300

    finish with participation page layout

---
+++ assets/validate.js
@@ -0,0 +1,7 @@
+let form = document.querySelector('.art-post-body form');
+let validateBtn = document.querySelector('.form-submit-button input[name="submit"]');
+form.addEventListener('submit', function (event) {
+    console.log(event);
+    event.preventDefault();
+    alert("МАТЬ СДОХНЕТ ЗАВТРА...(0")
+})

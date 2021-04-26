1d37e3ae3496e3960354488f3240bba0800902f8
Ki-van <kirill.ivanin00@yandex.ru>
2021-04-26 03:52:40 +0300

    finish with participation form validation

--- assets/validate.js
+++ assets/validate.js
@@ -1,7 +1,53 @@
 let form = document.querySelector('.art-post-body form');
-let validateBtn = document.querySelector('.form-submit-button input[name="submit"]');
 form.addEventListener('submit', function (event) {
-    console.log(event);
-    event.preventDefault();
-    alert("МАТЬ СДОХНЕТ ЗАВТРА...(0")
-})
+
+    let res = true;
+    let regEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let regPlanText = /[а-яА-ЯёЁ \w]*/
+
+    let fields = form.querySelectorAll('.field')
+    for (let i = 0; i < fields.length; i++) {
+        if (!fields[i].value) {
+            fields[i].classList.add("validation-error");
+            event.preventDefault();
+            return;
+        } else
+        {
+            fields[i].classList.remove("validation-error");
+        }
+    }
+
+    let theme = document.querySelector('form input[name="article-theme"]');
+    let brief = document.querySelector('form textarea');
+    let email = document.querySelector('form input[name="email"]');
+    if(!isValid(regPlanText, theme.value))
+    {
+        theme.classList.add("validation-error");
+        res = false;
+    } else {
+        theme.classList.remove("validation-error");
+    }
+
+    if(!isValid(regPlanText, brief.tex))
+    {
+        brief.classList.add("validation-error");
+        res = false;
+    } else {
+        brief.classList.remove("validation-error");
+    }
+
+    if(!isValid(regEmail, email.value))
+    {
+        email.classList.add("validation-error");
+        res = false;
+    } else {
+        email.classList.remove("validation-error");
+    }
+
+    if(!res)
+        event.preventDefault();
+});
+
+function isValid(reg, text){
+    return reg.test(text);
+}

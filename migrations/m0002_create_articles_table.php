ce6454d862315d1899650a4e064429a7877e5777
Ki-van <kirill.ivanin00@yandex.ru>
2021-07-03 20:10:07 +0300

    add status code column

--- migrations/m0002_create_articles_table.php
+++ migrations/m0002_create_articles_table.php
@@ -8,10 +8,11 @@ class m0002_create_articles_table
         $db->pdo->exec("
         CREATE TABLE articles (
             id INT AUTO_INCREMENT PRIMARY KEY,
-            author VARCHAR(255) REFERENCES users (id) ON DELETE SET NULL,
+            author_id VARCHAR(255) REFERENCES users (id) ON DELETE SET NULL,
             heading VARCHAR(255) NOT NULL,
             url VARCHAR(512) NOT NULL,
             description VARCHAR(512) NOT NULL ,
+            status_code INT NOT NULL,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             changed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         ) ENGINE=INNODB;

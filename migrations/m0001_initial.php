ed4401a97d4105f6b343afe677199d4139273ffc
Ki-van <kirill.ivanin00@yandex.ru>
2021-07-10 17:49:50 +0300

    add defaults to timestamps, basic roles and marks table

--- migrations/m0001_initial.php
+++ migrations/m0001_initial.php
@@ -5,25 +5,30 @@ class m0001_initial
     public function up()
     {
         $db = \app\core\Application::$app->db;
-        $db->pdo->exec(/** @lang PostgreSQL */ '            BEGIN;
-            CREATE TABLE public."user"    
-            (
-                id serial NOT NULL,
-                username character varying(255) NOT NULL,
-                email character varying(255) NOT NULL,
-                password character varying(512) NOT NULL,
-                created_at timestamp without time zone,
-                role_id integer NOT NULL,
-                PRIMARY KEY (id)
-            );
-            
-            CREATE TABLE public.role
+        $db->pdo->exec(/** @lang PostgreSQL */ '
+            BEGIN;
+             CREATE TABLE public.role
             (
                 id smallserial NOT NULL,
                 role_name character varying(63) NOT NULL,
                 PRIMARY KEY (id)
             );
             
+            insert into  public.role (role_name) values (\'Администратор\'), (\'Пользователь\');
+
+            CREATE TABLE public."user"    
+            (
+                id serial NOT NULL,
+                username character varying(255) NOT NULL,
+                email character varying(255) NOT NULL,
+                password character varying(512) NOT NULL,
+                created_at timestamp(0) without time zone NOT NULL default LOCALTIMESTAMP(0),
+                role_id integer NOT NULL REFERENCES public.role (id) ON DELETE RESTRICT,
+                PRIMARY KEY (id)
+            );
+            
+           
+
             CREATE TABLE public.evaluable
             (
                 id bigserial NOT NULL,
@@ -32,23 +37,23 @@ class m0001_initial
             
             CREATE TABLE public.article
             (
-                article_eval_id integer NOT NULL,
+                article_eval_id integer NOT NULL references public.evaluable (id) on DELETE restrict on update cascade,
                 heading character varying(255)[] NOT NULL,
                 description character varying(512)[] NOT NULL,
                 body text NOT NULL,
-                author_id integer,
-                created_at timestamp without time zone,
-                updated_at timestamp without time zone,
+                author_id integer REFERENCES public."user" (id) on delete set null on update cascade,
+                created_at timestamp(0) without time zone NOT NULL default LOCALTIMESTAMP(0),
+                updated_at timestamp(0) without time zone NOT NULL default LOCALTIMESTAMP(0),
                 PRIMARY KEY (article_eval_id)
             );
             
             CREATE TABLE public.comment
             (
-                comment_eval_id integer NOT NULL,
+                comment_eval_id integer NOT NULL REFERENCES public.evaluable (id) on delete restrict ,
                 body character varying(2047)[] NOT NULL,
-                author_id integer,
-                replay_eval_id bigint,
-                created_at timestamp without time zone,
+                author_id integer  REFERENCES public."user" (id) on delete set null ,
+                replay_eval_id bigint REFERENCES public.evaluable (id) on delete restrict ,
+                created_at timestamp(0) without time zone NOT NULL default LOCALTIMESTAMP(0),
                 PRIMARY KEY (comment_eval_id)
             );
             
@@ -61,57 +66,19 @@ class m0001_initial
             
             CREATE TABLE public.article_tag
             (
-                article_id integer NOT NULL,
-                tag_id integer NOT NULL
+                article_id integer NOT NULL REFERENCES public.article (article_eval_id) on delete cascade,
+                tag_id integer NOT NULL  REFERENCES public.tag (id) on delete restrict,
+                primary key (article_id, tag_id)
             );
             
-            ALTER TABLE public."user"
-                ADD FOREIGN KEY (role_id)
-                REFERENCES public.role (id)
-                NOT VALID;
-            
-            
-            ALTER TABLE public.article
-                ADD FOREIGN KEY (article_eval_id)
-                REFERENCES public.evaluable (id)
-                NOT VALID;
-            
-            
-            ALTER TABLE public.article
-                ADD FOREIGN KEY (author_id)
-                REFERENCES public."user" (id)
-                NOT VALID;
-            
-            
-            ALTER TABLE public.comment
-                ADD FOREIGN KEY (comment_eval_id)
-                REFERENCES public.evaluable (id)
-                NOT VALID;
-            
-            
-            ALTER TABLE public.comment
-                ADD FOREIGN KEY (author_id)
-                REFERENCES public."user" (id)
-                NOT VALID;
-            
-            
-            ALTER TABLE public.comment
-                ADD FOREIGN KEY (replay_eval_id)
-                REFERENCES public.evaluable (id)
-                NOT VALID;
-            
-            
-            ALTER TABLE public.article_tag
-                ADD FOREIGN KEY (article_id)
-                REFERENCES public.article (article_eval_id)
-                NOT VALID;
-            
-            
-            ALTER TABLE public.article_tag
-                ADD FOREIGN KEY (tag_id)
-                REFERENCES public.tag (id)
-                NOT VALID;
-            
+            create table public.marks
+            (
+              evaluable_id bigint not null references public.evaluable (id),
+              author_id integer  REFERENCES public."user" (id) on delete set null,
+              mark integer not null check ( mark in(-1, 1) ),
+              primary key (evaluable_id, author_id)
+            );
+
             END;
         ');
     }
@@ -120,12 +87,7 @@ class m0001_initial
     {
         $db = \app\core\Application::$app->db;
         $db->pdo->exec(/** @lang PostgreSQL*/'
-        DROP TABLE "user";
-        DROP TABLE role;
-        DROP TABLE article_tag;
-        DROP TABLE comment;
-        DROP TABLE tag;
-        DROP TABLE evaluable;
+        DROP SCHEMA public;
         ');
     }
 }
